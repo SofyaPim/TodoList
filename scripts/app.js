73bbbfb1c2f8765cf2733c6adc1ba6526dd0bf47
Sofya <48826470+SofyaPim@users.noreply.github.com>
2020-06-05 14:45:14 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,85 +1,71 @@
 
-var main = function () {
-        "use strict";
-        var toDos = [
-            "Закончить писать эту книгу",
-            "Вывести Грейси на прогулку в парк",
-            "Ответить на электронные письма",
-            "Подготовиться к лекции в понедельник",
-            "Обновить несколько новых задач",
-            "Купить продукты"
-        ];
-        var NewToDos = [
-            "Подготовиться к лекции на среду",
-            "Обновить гардероб",
-            "Купить фильм"
-        ];
-        var AddToDos = [
-            "learn jQuery",
-            "learn JS"
+let main = function () {
+    "use strict";
+    let toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты",
+        "купить собаку"
     ];
-        $(".tabs a span").toArray().forEach(function (element) {
-                
-                $(element).on("click", function () {
-                        var $element = $(element),
-                            $content;
-                        $(".tabs a span").removeClass("active");
-                        $element.addClass("active");
-                        $("main .content").empty();
-                        if ($element.parent().is(":nth-child(1)")) {
-                            $content = $("<ul>");
+    let NewToDos = [
+        "Подготовиться к лекции на среду",
+        "Обновить гардероб",
+        "Купить фильм"
+    ];
+    let AddToDos = [
 
-                            NewToDos.forEach(function (todo) {
-                                $content.append($("<li>").text(todo));
-                            });
-                            $("main .content").append($content)
+    ];
+    $(".tabs a span").toArray().forEach(function (element) {
 
-
-                        } else if ($element.parent().is(":nth-child(2)")) {
-                            $content = $("<ul>");
-
-                            toDos.forEach(function (todo) {
-                                $content.append($("<li>").text(todo));
-                            });
-                            $("main .content").append($content);
-
-                        } else if ($element.parent().is(":nth-child(3)")) {
-                             $content = $("<ul>");
-                             AddToDos.forEach(function (todo) {
-                                $content.append($("<li>").text(todo));
-                            });
-                            $("main .content").append($content);
-
-                            // $contentInput = $('<input type="text" class="inputVal">');
-                            // $contentButton = $('<button> + </button>')
-
-                            // $("main .content").append($contentInput);
-                            // $("main .content").append($contentButton);
-                            // if ($contentInput.val() !== "") {
-                            //     $content = $("<p>").text($("input").val());
-                            //     $("main .content").append($content);
-                            //     $contentInput.val("");
-
-                            //     $contentButton.on("click", function (event) {
-                            //         addCommentFromInputBox();
-                            //     });
-                            //     $contentInput.on("keypress", function (event) {
-                            //         if (event.keyCode === 13) {
-                            //             addCommentFromInputBox();
-                            //         }
-                              //  });
-                           // }
-                        
-                        };
-
-return false;
-
-            
-                    }
-
-
-                )  
+        $(element).on("click", function () {
+            let $element = $(element),
+                $content;
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                NewToDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
                 });
- $(".tabs a:first-child span").trigger("click");
-        };
-        $(document).ready(main);
+                $("main .content").append($content)
+
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
+
+            } else if ($element.parent().is(":nth-child(3)")) {
+
+                let but = $('<button class="addTodoButton">add todo</button>');
+                let inp = $("<input>").addClass('addToDoInput');
+                //inp.after(but);
+                $("main .content").append(inp);
+                $("main .content").append(but);
+                
+                $(but).on('click', function (params) {
+                    let newToDo = inp.val();
+                    //console.log(newToDo);
+                    let $content = $('<ul>');
+                    if (newToDo != '') {
+                        AddToDos.push($content.append($("<li>").text(newToDo)));
+                    }
+                    
+                    $("main .content").prepend($content);
+                    newToDo = inp.val('');
+                })
+
+
+
+            };
+            return false;
+        })
+    });
+    $(".tabs a:first-child span").trigger("click");
+};
+$(document).ready(main);
